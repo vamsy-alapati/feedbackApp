69dcee664e6a1800f08e5cde7db02215a365b7c7
VAMSY ALAPATI <alapati.vamsy1996@gmail.com>
2024-01-17 19:05:39 -0500

    corrected error

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -68,8 +68,10 @@ export const FeedbackProvider = ({ children }) => {
       body: JSON.stringify(updItem),
     });
 
+    const data = await response.json;
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
